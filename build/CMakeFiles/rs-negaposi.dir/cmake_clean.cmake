7d687a63a5eab85b9944a3285f6d077c58817c56
ChikaraHanakawa <s21c1095gc@s.chibakoudai.jp>
2023-03-17 18:00:10 +0900

    update

---
+++ build/CMakeFiles/rs-negaposi.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rs-negaposi.dir/rs-negaposi.cpp.o"
+  "rs-negaposi"
+  "rs-negaposi.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/rs-negaposi.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
