da0166fa4e4e6308d0679657709f2da5a1ded6dd
ChikaraHanakawa <s21c1095gc@s.chibakoudai.jp>
2023-03-19 20:51:22 +0900

    upload file

--- src/VideoCapture/rs-sobel.cpp
+++ src/VideoCapture/rs-sobel.cpp
@@ -16,13 +16,11 @@ int main(int argc, char **argv){
     cv::imshow("src", img_src);
 
     if(cv::waitKey(1) == 's'){
-      //cv::imwrite("test.jpg", img_src);
       cv::Sobel(img_src, img_tmp, CV_32F, 1, 0, 3);
       cv::convertScaleAbs(img_tmp, img_dst, 1, 0);
       cv::imwrite("sobel.jpg", img_dst);
     }
-
-    if(cv::waitKey(1) == 'q') break;
+    if(cv::waitKey(2) == 'q') break;
   }
 
   capture.release();
