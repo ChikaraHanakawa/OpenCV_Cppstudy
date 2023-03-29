bf5fd3968bc7fd0ac0635a406b3b89c64647ea0e
ChikaraHanakawa <s21c1095gc@s.chibakoudai.jp>
2023-03-29 21:43:50 +0900

    clean this repositories

---
+++ build/CMakeFiles/testbar.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/testbar.dir/testbar.cpp.o"
+  "testbar"
+  "testbar.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/testbar.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
